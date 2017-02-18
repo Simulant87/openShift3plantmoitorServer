273a77e876f49c6ed7eb11b23b55dd7093b6ef27
Simulant <xxx@yyy.com>
2017-02-18 11:00:39 +0100

    add request body to post service, add application/json mime type.

--- src/main/java/simulant/api/Ping.java
+++ src/main/java/simulant/api/Ping.java
@@ -1,11 +1,9 @@
 package simulant.api;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * A simple Ping Service to test if the Server is online.
@@ -24,12 +22,12 @@ public class Ping {
 	@RequestMapping(value = "/ping", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public @ResponseBody String getPing() {
-		return "{\"ping\": pong}";
+		return "{\"ping\": \"pong\"}";
 	}
 
-	@RequestMapping(value = "/pong", method = RequestMethod.POST)
+	@RequestMapping(value = "/pong", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.OK)
-	public @ResponseBody String postPong(String pong) {
+	public @ResponseBody String postPong(@RequestBody String pong) {
 		return pong;
 	}
 
