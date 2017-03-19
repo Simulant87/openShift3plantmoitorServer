191933f30087577eb7937ea8e58ab9bd1ae846ea
Simulant <xxx@yyy.com>
2017-03-19 12:53:39 +0100

    modified chart demo

--- src/main/webapp/chart.js
+++ src/main/webapp/chart.js
@@ -2,31 +2,27 @@ var canvas = document.getElementById('chart');
 new Chart(canvas, {
   type: 'line',
   data: {
-    labels: ['1', '2', '3', '4', '5'],
+    labels: ['1489922316000', '1489922436000', '1489922436000', '1489923035000'],
     datasets: [{
-      label: 'A',
-      yAxisID: 'A',
-      data: [100, 96, 84, 76, 69]
+      label: 'temperature in Celsius',
+      yAxisID: 'temperature',
+      data: [18.8, null, 20.8, 20.2]
     }, {
-      label: 'B',
-      yAxisID: 'B',
-      data: [1, 1, 1, 1, 0]
+      label: 'humidity in percentage',
+      yAxisID: 'humidity',
+      data: [60.99995559, 80.3456765, null, 35.23456]
     }]
   },
   options: {
     scales: {
       yAxes: [{
-        id: 'A',
+        id: 'temperature',
         type: 'linear',
         position: 'left',
       }, {
-        id: 'B',
+        id: 'humidity',
         type: 'linear',
-        position: 'right',
-        ticks: {
-          max: 1,
-          min: 0
-        }
+        position: 'right'
       }]
     }
   }
